7734f20c18d09d49951453886351825ba3e2b610
Riley Teige <riley.teige@gmail.com>
2012-10-29 01:03:25 +0000

    Added skeleton click event handlers for MainActivity.

--- MyHorseShow/src/edu/myhorseshow/MainActivity.java
+++ MyHorseShow/src/edu/myhorseshow/MainActivity.java
@@ -2,19 +2,66 @@ package edu.myhorseshow;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        setOnClickListeners();
     }
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    public void onClick(View clickedView)
+    {
+    	switch(clickedView.getId())
+    	{
+    	case R.id.main_screen_submit_button:
+    		submitLoginInfo();
+    		break;
+    	case R.id.main_screen_create_account_button:
+    		createAccount();
+    		break;
+    	default:
+    		Log.w(TAG, "Firing unimplemented button event.");
+    	}
+    }
+    
+    private void setOnClickListeners()
+    {
+    	Button submitButton = (Button) findViewById(R.id.main_screen_submit_button);
+    	Button createAccountButton = (Button) findViewById(R.id.main_screen_create_account_button);
+    	
+    	submitButton.setOnClickListener(this);
+    	createAccountButton.setOnClickListener(this);
+    }
+    
+    private void submitLoginInfo()
+    {
+    	sayNotImplemented("submitLoginInfo");
+    }
+    
+    private void createAccount()
+    {
+    	sayNotImplemented("createAccount");
+    }
+    
+    private static void sayNotImplemented(String what)
+    {
+    	Log.w(TAG, what + " has not been implemented yet.");
+    }
+    
+    private static final String TAG = "MainActivity";
 }
