cfc7a7ffae0e793fbf45f0a7cf2dc357b1a510d4
Riley Teige <riley.teige@gmail.com>
2012-10-29 18:54:13 +0000

    Preparing ListView adapters.

--- MyHorseShow/src/edu/myhorseshow/HomeActivity.java
+++ MyHorseShow/src/edu/myhorseshow/HomeActivity.java
@@ -2,6 +2,7 @@ package edu.myhorseshow;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class HomeActivity extends Activity
@@ -13,11 +14,20 @@ public class HomeActivity extends Activity
 		setContentView(R.layout.activity_home);
 		
 		welcomeUser(getIntent().getStringExtra(MainActivity.USERNAME));
+		setupListAdapters();
 	}
 	
 	private void welcomeUser(String username)
 	{
-		TextView headerTextView = (TextView) findViewById(R.id.activity_home_header_text_view);
+		TextView headerTextView = (TextView) findViewById(R.id.home_header_text_view);
 		headerTextView.setText(String.format(getString(R.string.welcome_header), username));
+	}
+	
+	private void setupListAdapters()
+	{
+		ListView upcomingEventsListView = (ListView) findViewById(R.id.home_upcoming_events_listview);
+		ListView alertsListView = (ListView) findViewById(R.id.home_alerts_listview);
+		
+		
 	}
 }
