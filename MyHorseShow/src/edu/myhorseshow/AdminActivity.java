d43ef805a19b0d081bd847cd798e3c361782db23
Riley Teige <riley.teige@gmail.com>
2012-10-30 20:13:57 +0000

    Forgot to add dark background button xml files. Also, added Admin, Instructor, Rider portals (activities).

---
+++ MyHorseShow/src/edu/myhorseshow/AdminActivity.java
@@ -0,0 +1,15 @@
+package edu.myhorseshow;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AdminActivity extends Activity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_admin);
+	}
+
+}
