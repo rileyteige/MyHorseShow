8f7fd21ac19298ec0f7c374b5bd815b5678ed1d2
Riley Teige <riley.teige@gmail.com>
2012-11-02 18:48:40 +0000

    Adding views to events. Colors will need work, but for now this will do.

--- MyHorseShow/src/edu/myhorseshow/EventActivity.java
+++ MyHorseShow/src/edu/myhorseshow/EventActivity.java
@@ -1,10 +1,14 @@
 package edu.myhorseshow;
 
+import edu.myhorseshow.R.id;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class EventActivity extends Activity
+public class EventActivity extends Activity implements OnClickListener
 {
 	public void onCreate(Bundle savedInstanceState)
 	{
@@ -13,5 +17,60 @@ public class EventActivity extends Activity
 		
 		TextView headerLabelTextView = (TextView) findViewById(R.id.event_header_label);
 		headerLabelTextView.setText("Event " + getIntent().getLongExtra(HomeActivity.EVENT_ID, 0));
+		
+		setupClickListeners();
 	}
+
+	@Override
+	public void onClick(View clickedView) {
+		switch(clickedView.getId())
+		{
+		case R.id.event_class_list_button:
+			makeViewVisible(R.id.event_class_list_view);
+			break;
+		case R.id.event_ride_times_button:
+			makeViewVisible(R.id.event_ride_times_view);
+			break;
+		case R.id.event_barn_info_button:
+			makeViewVisible(R.id.event_barn_info_view);
+			break;
+		case R.id.event_contact_info_button:
+			makeViewVisible(R.id.event_contact_info_view);
+			break;
+		}
+	}
+	
+	private void setupClickListeners()
+	{
+		((Button) findViewById(R.id.event_class_list_button)).setOnClickListener(this);
+		((Button) findViewById(R.id.event_ride_times_button)).setOnClickListener(this);
+		((Button) findViewById(R.id.event_barn_info_button)).setOnClickListener(this);
+		((Button) findViewById(R.id.event_contact_info_button)).setOnClickListener(this);
+	}
+	
+	private void makeViewVisible(int viewId)
+	{
+		View nextView = findViewById(viewId);
+		if (nextView == null)
+			return;
+		
+		View currentView = getCurrentView();
+		if (currentView != null)
+			currentView.setVisibility(View.INVISIBLE);
+		
+		nextView.setVisibility(View.VISIBLE);
+		setCurrentView(nextView);
+	}
+	
+	private View getCurrentView()
+	{
+		return mCurrentView;
+	}
+	
+	private void setCurrentView(View view)
+	{
+		mCurrentView = view;
+	}
+	
+	private View mCurrentView;
 }
