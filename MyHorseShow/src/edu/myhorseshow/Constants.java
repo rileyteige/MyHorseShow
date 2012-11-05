3c35fe9a30edab2ffb03ad52d2950a04545285aa
Riley Teige <riley.teige@gmail.com>
2012-11-05 19:04:15 +0000

    Adding a constants class for localization.

---
+++ MyHorseShow/src/edu/myhorseshow/Constants.java
@@ -0,0 +1,8 @@
+package edu.myhorseshow;
+
+public final class Constants
+{
+	public static int INVALID_LOGIN_CODE = -1;
+	public static String EMAIL_ADDR_PARAM = "addr";
+	public static String PASSWORD_PARAM = "p";
+}
