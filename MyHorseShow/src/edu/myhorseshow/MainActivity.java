dff3471c4b86d02415708358344231e43fb07549
Riley Teige <riley.teige@gmail.com>
2012-11-09 19:48:54 +0000

    Removing unused import.

--- MyHorseShow/src/edu/myhorseshow/MainActivity.java
+++ MyHorseShow/src/edu/myhorseshow/MainActivity.java
@@ -6,7 +6,6 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
