3e8ccfe61fd4513aee6ac731adcce4120a8da416
Riley Teige <riley.teige@gmail.com>
2012-11-30 10:33:27 +0000

    ShowClasses and Participations notify changes.

--- MyHorseShow/src/edu/myhorseshow/models/Participation.java
+++ MyHorseShow/src/edu/myhorseshow/models/Participation.java
@@ -1,8 +1,17 @@
 package edu.myhorseshow.models;
 
+import edu.myhorseshow.events.EventDispatcher;
 
-public class Participation
+
+public class Participation extends EventDispatcher
 {
+	public static class EventMeta
+	{
+		public static final String RANK_CHANGED = getFullName("RANK_CHANGED");
+		public static final String HORSE_CHANGED = getFullName("HORSE_CHANGED");
+		private static String getFullName(String str) { return EventMeta.class.getName() + str; }
+	}
+	
 	public long getId() { return id; }
 	public User getRider() { return rider; }
 	public String getHorse() { return horse; }
@@ -10,8 +19,18 @@ public class Participation
 	
 	public void setId(long id) { this.id = id; }
 	public void setUser(User rider) { this.rider = rider; }
-	public void setHorse(String horse) { this.horse = horse; }
-	public void setRank(int rank) { this.rank = rank; }
+	
+	public void setHorse(String horse)
+	{
+		this.horse = horse;
+		notifyChange(EventMeta.HORSE_CHANGED);
+	}
+	
+	public void setRank(int rank)
+	{
+		this.rank = rank;
+		notifyChange(EventMeta.RANK_CHANGED);
+	}
 	
 	private long id;
 	private User rider;
--- MyHorseShow/src/edu/myhorseshow/models/ShowClass.java
+++ MyHorseShow/src/edu/myhorseshow/models/ShowClass.java
@@ -1,17 +1,45 @@
 package edu.myhorseshow.models;
 
+import edu.myhorseshow.events.EventDispatcher;
 
-public class ShowClass implements NamedObject
+
+public class ShowClass extends EventDispatcher implements NamedObject
 {
+	public static class EventMeta
+	{
+		public static final String NAME_CHANGED = getFullName("NAME_CHANGED");
+		public static final String START_TIME_CHANGED = getFullName("START_TIME_CHANGED");
+		public static final String PARTICIPANTS_CHANGED = getFullName("PARTICIPANTS_CHANGED");
+		private static String getFullName(String str) { return EventMeta.class.getName() + str; }
+	}
+	
 	public long getId() { return id; }
 	public String getName() { return name; }
 	public String getStartTime() { return starttime; }
 	public Participation[] getParticipations() { return ownParticipation; }
 	
-	public void setId(long id) { this.id = id; }
-	public void setName(String name) { this.name = name; }
-	public void setStartTime(String time) { this.starttime = time; }
-	public void setParticipations(Participation[] participations) { this.ownParticipation = participations; }
+	public void setId(long id)
+	{
+		this.id = id;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+		notifyChange(EventMeta.NAME_CHANGED);
+	}
+	
+	public void setStartTime(String time)
+	{
+		this.starttime = time;
+		notifyChange(EventMeta.START_TIME_CHANGED);
+	}
+	
+	public void setParticipations(Participation[] participations)
+	{
+		this.ownParticipation = participations;
+		notifyChange(EventMeta.PARTICIPANTS_CHANGED);
+	}
 	
 	private long id;
 	private String name;
