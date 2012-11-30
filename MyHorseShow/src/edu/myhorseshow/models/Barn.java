240885958ddb289ab1a2a0bf0f14bd95f13d8d10
Riley Teige <riley.teige@gmail.com>
2012-11-30 10:41:11 +0000

    Barns dispatching events.

--- MyHorseShow/src/edu/myhorseshow/models/Barn.java
+++ MyHorseShow/src/edu/myhorseshow/models/Barn.java
@@ -1,15 +1,34 @@
 package edu.myhorseshow.models;
 
+import edu.myhorseshow.events.EventDispatcher;
 
-public class Barn implements NamedObject
+
+public class Barn extends EventDispatcher implements NamedObject
 {
+	public static class EventMeta
+	{
+		public static String NAME_CHANGED = getFullName("NAME_CHANGED");
+		public static String STALLS_CHANGED = getFullName("STALLS_CHANGED");
+		private static String getFullName(String str) { return EventMeta.class.getName() + str; }
+	}
+	
 	public int getId() { return id; }
 	public String getName() { return name; }
 	public Stall[] getStalls() { return ownStall; }
-	
+
 	public void setId(int id) { this.id = id; }
-	public void setName(String name) { this.name = name; }
-	public void setStalls(Stall[] stalls) { this.ownStall = stalls; }
+	
+	public void setName(String name) 
+	{
+		this.name = name;
+		notifyChange(EventMeta.NAME_CHANGED);
+	}
+	
+	public void setStalls(Stall[] stalls) 
+	{
+		this.ownStall = stalls;
+		notifyChange(EventMeta.STALLS_CHANGED);
+	}
 	
 	private int id;
 	private String name;
