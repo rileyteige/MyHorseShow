40741ff4fc20cbf2c46cf59dbf8e6be5b9489cbb
Riley Teige <riley.teige@gmail.com>
2012-11-30 10:35:52 +0000

    Divisions are dispatching events.

--- MyHorseShow/src/edu/myhorseshow/models/Division.java
+++ MyHorseShow/src/edu/myhorseshow/models/Division.java
@@ -1,13 +1,30 @@
 package edu.myhorseshow.models;
 
+import edu.myhorseshow.events.EventDispatcher;
 
-public class Division implements NamedObject
+
+public class Division extends EventDispatcher implements NamedObject
 {
+	public static class EventMeta
+	{
+		public static final String NAME_CHANGED = getFullName("NAME_CHANGED");
+		public static final String CLASSES_CHANGED = getFullName("CLASSES_CHANGED");
+		private static String getFullName(String str) { return EventMeta.class.getName() + str; }
+	}
+	
 	public String getName() { return name; }
-	public void setName(String name) { this.name = name; }
+	public void setName(String name)
+	{ 
+		this.name = name;
+		notifyChange(EventMeta.NAME_CHANGED);
+	}
 	
 	public ShowClass[] getClasses() { return ownClass; }
-	public void setClasses(ShowClass[] classes) { this.ownClass = classes; }
+	public void setClasses(ShowClass[] classes)
+	{
+		this.ownClass = classes;
+		notifyChange(EventMeta.CLASSES_CHANGED);
+	}
 	
 	private String name;
 	private ShowClass[] ownClass;
