2dfb3d18181709b13cef9d8c2c7b834a19945f5f
Riley Teige <riley.teige@gmail.com>
2012-11-30 10:38:49 +0000

    Contacts dispatching events.

--- MyHorseShow/src/edu/myhorseshow/models/Contact.java
+++ MyHorseShow/src/edu/myhorseshow/models/Contact.java
@@ -1,8 +1,19 @@
 package edu.myhorseshow.models;
 
+import edu.myhorseshow.events.EventDispatcher;
 
-public class Contact implements NamedObject
+
+public class Contact extends EventDispatcher implements NamedObject
 {
+	public static class EventMeta
+	{
+		public static final String NAME_CHANGED = getFullName("NAME_CHANGED");
+		public static final String EMAIL_CHANGED = getFullName("EMAIL_CHANGED");
+		public static final String PHONE_CHANGED = getFullName("PHONE_CHANGED");
+		public static final String OCCUPATION_CHANGED = getFullName("OCCUPATION_CHANGED");
+		private static String getFullName(String str) { return EventMeta.class.getName() + str; }
+	}
+	
 	/** Returns the full name of a contact. */
 	public String getName()
 	{
@@ -16,19 +27,39 @@ public class Contact implements NamedObject
 	public void setId(int id) { this.id = id; }
 	
 	public String getFirstName() { return firstname; }
-	public void setFirstName(String name) { firstname = name; }
+	public void setFirstName(String name)
+	{
+		firstname = name;
+		notifyChange(EventMeta.NAME_CHANGED);
+	}
 	
 	public String getLastName() { return lastname; }
-	public void setLastName(String name) { lastname = name; }
+	public void setLastName(String name)
+	{
+		lastname = name;
+		notifyChange(EventMeta.NAME_CHANGED);
+	}
 	
 	public String getEmail() { return email; }
-	public void setEmail(String email) { this.email = email; }
+	public void setEmail(String email)
+	{
+		this.email = email;
+		notifyChange(EventMeta.EMAIL_CHANGED);
+	}
 	
 	public String getPhoneNumber() { return phone; }
-	public void setPhoneNumber(String phone) { this.phone = phone; }
+	public void setPhoneNumber(String phone)
+	{
+		this.phone = phone;
+		notifyChange(EventMeta.PHONE_CHANGED);
+	}
 	
 	public Occupation getOccupation() { return occupation; }
-	public void setOccupation(Occupation occupation) { this.occupation = occupation; }
+	public void setOccupation(Occupation occupation)
+	{
+		this.occupation = occupation;
+		notifyChange(EventMeta.OCCUPATION_CHANGED);
+	}
 	
 	private int id;
 	private String firstname;
